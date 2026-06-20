96f96e5cc4a59b82817ec60f37cc7929b5ab333e
Nithiya-25 <25ad105@rmkcet.ac.in>
2026-06-21 00:02:42 +0530

    Modified code

--- ATM_Withdrawal.py
+++ ATM_Withdrawal.py
@@ -1,6 +1,10 @@
+#ATM Withdrawal
 balance = int(input("Enter your balance : "))
 amount = int(input("Enter the amount to withdraw : "))
-if amount <= balance:
+if amount <= 0:
+    print("Invalid amount")
+elif amount <= balance:
     print("Withdraw successful")
+    print("Remaining amount : ", balance-amount)
 else:
     print("Insufficient balance")
